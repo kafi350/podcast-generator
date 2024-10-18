226342160be0c0bdb6d95eefbf46fd9e1d4b3570
Abdullahil Kafi <kafiabdullahil96@gmail.com>
2024-10-18 13:47:15 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 echo "========"
 
 git config --global user.name kafi350
-git config --global user.eamil "kafiabdullahil96@gmail.com"
+git config --global user.eamil kafiabdullahil96@gmail.com
 git config --global --add safe.directory /github/workspace
 
 python3 /usr/bin/feed.py
